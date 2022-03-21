c60e779392a42ccfb1101bf9288c1086ef06ac7e
Kagemand Andersen <trillykins@gmail.com>
2022-03-21 19:38:34 +0100

    Add on/off functionality

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var enabled = true;
+
 function renameYouTubePage(tabId) {
   const searchPattern = new RegExp(/^\([0-9]+\)/g);
   const result = searchPattern.exec(document.title);
@@ -9,6 +11,7 @@ function renameYouTubePage(tabId) {
 }
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (!enabled) return;
   if (['chrome://', 'edge://'].some(x => !tab.url.includes(x))) {
     if (['https://www.youtube.com', 'https://youtube.com', 'https://www.youtu.be', 'https://youtu.be'].some(y => tab.url.toLowerCase().startsWith(y))) {
       chrome.scripting.executeScript({
@@ -17,5 +20,14 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         args: [tabId]
       });
     }
+  }
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  enabled = !enabled;
+  if (!enabled) {
+    chrome.action.setBadgeText({ text: 'off' });
+  } else {
+    chrome.action.setBadgeText({ text: '' });
   }
 });
