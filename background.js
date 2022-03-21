42bfbb5ce719467f0340c6ded21fbce2859a6ca9
Kagemand Andersen <trillykins@gmail.com>
2022-03-21 16:34:41 +0100

    V1

---
+++ background.js
@@ -0,0 +1,21 @@
+function renameYouTubePage(tabId) {
+  const searchPattern = new RegExp(/^\([0-9]+\)/g);
+  const result = searchPattern.exec(document.title);
+  if (result) {
+    document.title = `${result[0]} YouTube ${tabId}`;
+  } else {
+    document.title = `YouTube ${tabId}`;
+  }
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (['chrome://', 'edge://'].some(x => !tab.url.includes(x))) {
+    if (['https://www.youtube.com', 'https://youtube.com', 'https://www.youtu.be', 'https://youtu.be'].some(y => tab.url.toLowerCase().startsWith(y))) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: renameYouTubePage,
+        args: [tabId]
+      });
+    }
+  }
+});
