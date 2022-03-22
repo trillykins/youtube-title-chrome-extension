e7cc8908d485f739ac9e751d0797536e9a50882f
Kagemand Andersen <trillykins@gmail.com>
2022-03-22 14:45:33 +0100

    Use storage to revert to original YouTube titles

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
+const urls = ['https://www.youtube.com', 'https://youtube.com', 'https://www.youtu.be', 'https://youtu.be'];
 var enabled = true;
-var map = new Map();
 
 function renameYouTubePage(tabId, tabTitle) {
-  if (tabTitle !== null) {
+  if (tabTitle != null) {
     document.title = tabTitle;
     return;
   }
-  if (tabTitle === null) tabTitle = document.title;
-  const searchPattern = new RegExp(/^\([0-9]+\)/g);
-  const result = searchPattern.exec(tabTitle);
+  const notificationSearchPattern = new RegExp(/^\([0-9]+\)/g);
+  const result = notificationSearchPattern.exec(document.title);
   if (result) {
     document.title = `${result[0]} YouTube ${tabId}`;
   } else {
@@ -16,15 +15,22 @@ function renameYouTubePage(tabId, tabTitle) {
   }
 }
 
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+  chrome.storage.local.get(`${tab.id}`, function (data) {
+    if (Object.keys(data).length === 0 && data.constructor === Object) return;
+    console.log("Removing tab id: " + tabId);
+    chrome.stoage.local.remove(removedTabId);
+  });
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(map);
   if (!enabled) return;
-  if (['https://www.youtube.com', 'https://youtube.com', 'https://www.youtu.be', 'https://youtu.be'].some(y => tab.url.startsWith(y))) {
-
-    if (!map.has(tab.id)) {
-      map.set(tabId, tab.title);
+  if (urls.some(y => tab.url.startsWith(y))) {
+    const hiddenTitleSearchPattern = new RegExp(/^(\([0-9]+\)\s)?(YouTube)\s[0-9]+/g);
+    if (!hiddenTitleSearchPattern.test(tab.title)) {
+      console.log("Saving: " + [tab.id] + ", " + [tab.title]);
+      chrome.storage.local.set({ [tab.id]: tab.title });
     }
-
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: renameYouTubePage,
@@ -33,34 +39,47 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 });
 
-chrome.action.onClicked.addListener((tab) => {
+chrome.tabs.onReplaced.addListener((addedTabId, removedTabId) => {
+  chrome.storage.local.get(`${tab.id}`, function (data) {
+    if (Object.keys(data).length === 0 && data.constructor === Object) return;
+    console.log("Tab ID is getting replaced: " + removedTabId + " => " + addedTabId);
+    chrome.stoage.local.remove(removedTabId);
+    chrome.storage.local.set({ [addedTabId]: tab.title });
+  });
+});
+
+chrome.action.onClicked.addListener(() => {
   enabled = !enabled;
   if (enabled) {
     chrome.action.setBadgeText({ text: '' });
-    chrome.tabs.query({}, function (tabs) {
-      tabs.forEach(function (tab) {
-        if (map.has(tab.id)) {
+  } else {
+    chrome.action.setBadgeText({ text: 'off' });
+  }
+
+  chrome.tabs.query({}, function (tabs) {
+    tabs.forEach(function (tab) {
+      if (urls.some(u => tab.url.startsWith(u))) {
+        if (enabled) {
+          console.log(tab.id + " is going into hiding!");
           chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: renameYouTubePage,
             args: [tab.id, null]
           });
-        }
-      });
-    });
-  } else {
-    chrome.action.setBadgeText({ text: 'off' });
-    chrome.tabs.query({}, function (tabs) {
-      tabs.forEach(function (tab) {
-        if (map.has(tab.id)) {
-          chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: renameYouTubePage,
-            args: [tab.id, map.get(tab.id)]
+        } else {
+          chrome.storage.local.get(`${tab.id}`, function (data) {
+            if (data[tab.id] === undefined) console.log("data is undefined");
+            else {
+              console.log(tab.id + " => " + data[tab.id]);
+              chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                function: renameYouTubePage,
+                args: [tab.id, data[tab.id]]
+              });
+            }
           });
         }
-      });
+      }
     });
-  }
-  console.log("...");
+  });
 });
